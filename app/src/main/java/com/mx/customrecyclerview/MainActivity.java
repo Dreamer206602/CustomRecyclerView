a77ad500d0d9cdae9be6fba28335f743c96e1ca1
Deamer206602 <boobooMX@163.com>
2016-06-02 09:42:49 +0800

    add some files

--- app/src/main/java/com/mx/customrecyclerview/MainActivity.java
+++ app/src/main/java/com/mx/customrecyclerview/MainActivity.java
@@ -1,20 +1,73 @@
 package com.mx.customrecyclerview;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.zhy.base.adapter.ViewHolder;
+import com.zhy.base.adapter.recyclerview.CommonAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recyclerView)
+    CustomeRecyclerView mRecyclerView;
+    @Bind(R.id.recyclerView2)
+    CustomeRecyclerView mRecyclerView2;
+    @Bind(R.id.recyclerView3)
+    CustomeRecyclerView mRecyclerView3;
+    private List<Integer> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mDatas = new ArrayList<>(Arrays.asList(
+                R.mipmap.a,
+                R.mipmap.b,
+                R.mipmap.c,
+                R.mipmap.d,
+                R.mipmap.e,
+                R.mipmap.f,
+                R.mipmap.g,
+                R.mipmap.h,
+                R.mipmap.l));
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView2.setLayoutManager(linearLayoutManager2);
+
+
+        mRecyclerView.setAdapter(new CommonAdapter<Integer>(this, R.layout.item_data, mDatas) {
+            @Override
+            public void convert(ViewHolder holder, final Integer integer) {
+                holder.setImageResource(R.id.id_index_gallery_item_image, integer);
+
+
+            }
+        });
+        mRecyclerView2.setAdapter(new CommonAdapter<Integer>(this, R.layout.item_data, mDatas) {
+            @Override
+            public void convert(ViewHolder holder, final Integer integer) {
+                holder.setImageResource(R.id.id_index_gallery_item_image, integer);
+
+            }
+        });
+
+
 
     }
 
